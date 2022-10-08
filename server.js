c02d22c9f850d951330d810f58f78503e7a6807d
A2v10 <hey.a2v10@gmail.com>
2022-10-08 22:04:46 +0530

    Login Bot To Server || Basic Message Reply

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("Init")
+//import dotemv
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", async (message) => {
+  if(message.content==='!ping'){
+    message.reply('pong');
+  }
+})
+
+client.login(process.env.TOKEN)
